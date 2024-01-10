656449f6093b33ee87847fe562e48e44c317630f
vaibhav03ambhore <vaibhavambhore803@gmail.com>
2024-01-10 19:11:08 +0530

    event dropdown and search bar css

--- src/components/Event_Page/Header.jsx
+++ src/components/Event_Page/Header.jsx
@@ -1,16 +1,33 @@
-import React from 'react';
+import React ,{useState,useEffect} from 'react';
 import '../../pages_css/Event_Page_CSS/Header.css';
 import SearchBar from './SearchBar';
 import Dropdown from './Dropdown';
+
 
 const Header = ({ scrollToSection , searchQuery , onSearchInputChange}) => {
   const handleDropdownChange = selectedOption => {
     scrollToSection(selectedOption.value);
   };
 
+
+  const [isScrolled,setIsScrolled]=useState(false);
+
+  useEffect(()=>{
+    const scrollHandler=()=>{
+      window.scrollY>80?setIsScrolled(true):setIsScrolled(false);
+    };
+
+    window.addEventListener('scroll',scrollHandler);
+
+    return()=>{
+      window.removeEventListener('scroll', scrollHandler);
+    };
+  },[]);
+
   return (
-    <div className="e-header-container">
-      <div className="e-header">
+    <div className={`header-container ${isScrolled?'scrolled':''}`}>
+    {/* // <div className="header-container "> */}
+      <div className="header">
         <Dropdown onChange={handleDropdownChange} />
         <SearchBar 
            searchQuery={searchQuery}
