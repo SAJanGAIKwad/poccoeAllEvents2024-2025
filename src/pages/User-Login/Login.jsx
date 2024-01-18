84d59ee198a1f1278b9f0668acd7fa88120882b4
SAJanGAIKwad <sajan.gaikwad21@pccoepune.org>
2024-01-18 23:16:41 +0530

    Form Validation Improved

--- src/pages/User-Login/Login.jsx
+++ src/pages/User-Login/Login.jsx
@@ -7,6 +7,8 @@ import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Link, Navigate } from 'react-router-dom';
 import { UserContext } from "../../UserContext";
+import { MdMail } from 'react-icons/md';
+import { RiLockPasswordFill } from 'react-icons/ri';
 const Login = () => {
     function showAlert1() {
         toast.success("Login Successfully");
@@ -66,36 +68,44 @@ const Login = () => {
             <div className="mt-4 grow flex items-center justify-around ">
                 <div className="mb-64 shadow-2xl rounded-lg p-10 ">
                     <h1 className="text-4xl text-center mb-4">Login</h1>
-                    <form className="max-w-sm mx-auto" onSubmit={handleSubmit}>
-                        <p className='text-rose-700'>{formErrors.email}</p>
-                        <input type="email"
-                            placeholder="your@email.com"
-                            value={email}
-                            onChange={ev => setEmail(ev.target.value)} />
-                        <p className='text-rose-700'>{formErrors.password}</p>
-                        <input type="password"
-                            placeholder="password"
-                            value={password}
-                            onChange={ev => setPassword(ev.target.value)} />
-                        <button className="primary">Login</button>
-                        <div className='py-3 flex justify-center text-sky-600 font-bold'>
-                            <Link to={'/forgot-password'}>Forgot Password</Link>
-                        </div>
-                        <GoogleOAuthProvider className='m-10 bg-danger' clientId="446061591121-opbuk29t8r85tdq2uia6am4m54ioqleh.apps.googleusercontent.com">
-                            <div className='m-5 flex justify-center'>
-                                <GoogleLogin
-                                    onSuccess={credentialResponse => {
-                                        var decoded = jwt_decode(credentialResponse.credential);
-                                        console.log(decoded);
-                                    }}
-                                    onError={() => {
-                                        console.log('Login Failed')
-                                    }}
-                                />
+                    <form className="max-w-md mx-auto" onSubmit={handleSubmit}>
+                        <div className='w-[400px]'>
+                            <div className='flex items-center rounded-2xl'>
+                                <MdMail className='mx-2'/>
+                                <input type="email"
+                                    placeholder="your@email.com"
+                                    value={email}
+                                    onChange={ev => setEmail(ev.target.value)} />
                             </div>
-                        </GoogleOAuthProvider>
-                        <div className="text-center py-2 text-gray-500">
-                            Don't have an account yet? <Link className="underline text-black" to={'/user-register'}>Register now</Link>
+                            <p className='text-rose-700'>{formErrors.email}</p>
+                            <div className='flex items-center rounded-2xl'>
+                                <RiLockPasswordFill className='mx-2' />
+                                <input type="password"
+                                    placeholder="password"
+                                    value={password}
+                                    onChange={ev => setPassword(ev.target.value)} />
+                            </div>
+                            <p className='text-rose-700'>{formErrors.password}</p>
+                            <button className="primary">Login</button>
+                            <div className='py-3 flex justify-center text-sky-600 font-bold'>
+                                <Link to={'/forgot-password'}>Forgot Password</Link>
+                            </div>
+                            <GoogleOAuthProvider className='m-10 bg-danger' clientId="446061591121-opbuk29t8r85tdq2uia6am4m54ioqleh.apps.googleusercontent.com">
+                                <div className='m-5 flex justify-center'>
+                                    <GoogleLogin
+                                        onSuccess={credentialResponse => {
+                                            var decoded = jwt_decode(credentialResponse.credential);
+                                            console.log(decoded);
+                                        }}
+                                        onError={() => {
+                                            console.log('Login Failed')
+                                        }}
+                                    />
+                                </div>
+                            </GoogleOAuthProvider>
+                            <div className="text-center py-2 text-gray-500">
+                                Don't have an account yet? <Link className="underline text-black" to={'/user-register'}>Register now</Link>
+                            </div>
                         </div>
                     </form>
                 </div>
