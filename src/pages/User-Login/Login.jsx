f980a523607a084867771b64bc8bb3511a8e04c5
SAJanGAIKwad <sajan.gaikwad21@pccoepune.org>
2024-01-18 22:58:43 +0530

    Form Validation Improved

--- src/pages/User-Login/Login.jsx
+++ src/pages/User-Login/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useState, useContext } from 'react'
 import '../../App.css';
 import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 import jwt_decode from "jwt-decode";
@@ -8,29 +8,47 @@ import 'react-toastify/dist/ReactToastify.css';
 import { Link, Navigate } from 'react-router-dom';
 import { UserContext } from "../../UserContext";
 const Login = () => {
-    function showAlert() {
-        toast.error("Please Check Credentials");
+    function showAlert1() {
+        toast.success("Login Successfully");
     }
+    // function showAlert2() {
+    //     toast.error("Login Failed Plz Check Credentials");
+    // }
     const [email, setEmail] = useState()
     const [password, setPassword] = useState()
-    const { setUser } = useContext(UserContext);
     const [redirect, setRedirect] = useState(false);
+    const [formErrors, setFormErrors] = useState({});
+    const { setUser } = useContext(UserContext);
 
     async function handleSubmit(ev) {
-        ev.preventDefault()
-        try {
-            const { data } = await axios.post('http://localhost:3001/login', { email, password });
-            setUser(data);
-            setRedirect(true);
-
-        } catch (e) {
-            alert('Login failed. Please check credentails');
-        }
-
+        ev.preventDefault();
+        setFormErrors(validate(email, password));
+        const { data } = await axios.post('http://localhost:3001/login', { email, password });
+        setUser(data);
+        showAlert1();
+        setRedirect(true);
         if (redirect) {
             return <Navigate to={'/'} />
         }
-    }
+    };
+
+    const validate = (email, password) => {
+        const errors = {};
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+        if (!email) {
+            errors.email = "Email is required!";
+        } else if (!regex.test(email)) {
+            errors.email = "This is not a valid email format!";
+        }
+        if (!password) {
+            errors.password = "Password is required";
+        } else if (password.length < 4) {
+            errors.password = "Password must be more than 4 characters";
+        } else if (password.length > 10) {
+            errors.password = "Password cannot exceed more than 10 characters";
+        }
+        return errors;
+    };
     return (
         <>
             <ToastContainer
@@ -49,15 +67,17 @@ const Login = () => {
                 <div className="mb-64 shadow-2xl rounded-lg p-10 ">
                     <h1 className="text-4xl text-center mb-4">Login</h1>
                     <form className="max-w-sm mx-auto" onSubmit={handleSubmit}>
+                        <p className='text-rose-700'>{formErrors.email}</p>
                         <input type="email"
                             placeholder="your@email.com"
                             value={email}
                             onChange={ev => setEmail(ev.target.value)} />
+                        <p className='text-rose-700'>{formErrors.password}</p>
                         <input type="password"
                             placeholder="password"
                             value={password}
                             onChange={ev => setPassword(ev.target.value)} />
-                        <button className="primary" onClick={showAlert}>Login</button>
+                        <button className="primary">Login</button>
                         <div className='py-3 flex justify-center text-sky-600 font-bold'>
                             <Link to={'/forgot-password'}>Forgot Password</Link>
                         </div>
