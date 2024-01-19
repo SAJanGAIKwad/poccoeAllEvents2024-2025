a498a61696675f907e2ed8540ee4a1d99f0d84f0
SAJanGAIKwad <sajan.gaikwad21@pccoepune.org>
2024-01-19 21:46:15 +0530

    User & Admin Routes issue Solved

--- src/pages/Admin-Login/Login.jsx
+++ src/pages/Admin-Login/Login.jsx
@@ -1,75 +1,97 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import '../../App.css';
-import * as Components from '../../Components';
 import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 import jwt_decode from "jwt-decode";
-import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
-import { ToastContainer, toast } from 'react-toastify';
+import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-const Adminlogin = () => {
+import { Link, Navigate } from 'react-router-dom';
+import { UserContext } from "../../UserContext";
+import { MdMail } from 'react-icons/md';
+import { RiLockPasswordFill } from 'react-icons/ri';
+const Login = () => {
     function showAlert1() {
-        toast.success("SuccessFully! Registered");
+        toast.success("Login Successfully");
     }
-    function showError() {
-        toast.error('Please Check Email and Password Again');
-    }
-    const [name, setName] = useState()
+    // function showAlert2() {
+    //     toast.error("Login Failed Plz Check Credentials");
+    // }
     const [email, setEmail] = useState()
-    const [mobile, setMobile] = useState()
     const [password, setPassword] = useState()
-    const navigate = useNavigate()
-    const handleSubmit1 = (e) => {
-        e.preventDefault()
-        axios.post('http://localhost:3001/register', { name, email, mobile, password })
-            .then(result => {
-                console.log(result)
-                navigate('/')
-            })
-            .catch(err => console.log(err))
-    }
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        axios.post('http://localhost:3001/login', { email, password })
-            .then(result => {
-                console.log(result)
-                if (result.data === "Success") {
-                    navigate('/event')
-                }
-                else {
-                    showError()
-                }
-            })
-            .catch(err =>
-                console.log(err),
-            )
-    }
-    const [signIn, toggle] = React.useState(true);
+    const [redirect, setRedirect] = useState(false);
+    const [formErrors, setFormErrors] = useState({});
+    const { setUser } = useContext(UserContext);
+
+    async function handleSubmit(ev) {
+        ev.preventDefault();
+        setFormErrors(validate(email, password));
+        const { data } = await axios.post('http://localhost:3001/login', { email, password });
+        setUser(data);
+        showAlert1();
+        setRedirect(true);
+        if (redirect) {
+            return <Navigate to={'/'} />
+        }
+    };
+
+    const validate = (email, password) => {
+        const errors = {};
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+        if (!email) {
+            errors.email = "Email is required!";
+        } else if (!regex.test(email)) {
+            errors.email = "This is not a valid email format!";
+        }
+        if (!password) {
+            errors.password = "Password is required";
+        } else if (password.length < 4) {
+            errors.password = "Password must be more than 4 characters";
+        } else if (password.length > 10) {
+            errors.password = "Password cannot exceed more than 10 characters";
+        }
+        return errors;
+    };
     return (
         <>
-            <Components.Mcontainer>
-                <ToastContainer/>
-                <Components.Container className='Cbody'>
-                    <Components.SignUpContainer signinIn={signIn}>
-                        <Components.Form onSubmit={handleSubmit1}>
-                            <Components.Title >Create Account</Components.Title>
-                            <Components.Input type='text' placeholder='Name' onChange={(e) => setName(e.target.value)} required />
-                            <Components.Input type='email' placeholder='Email' onChange={(e) => setEmail(e.target.value)} required />
-                            <Components.Input type='mobile' placeholder='Mobile' onChange={(e) => setMobile(e.target.value)} required />
-                            <Components.Input type='password' placeholder='Password' onChange={(e) => setPassword(e.target.value)} required />
-                            <Components.Button onClick={showAlert1}>Sign Up</Components.Button>
-                        </Components.Form>
-                    </Components.SignUpContainer>
-
-                    <Components.SignInContainer signinIn={signIn}>
-                        <Components.Form onSubmit={handleSubmit}>
-                            <Components.Title>Sign in</Components.Title>
-                            <Components.Input type='email' placeholder='Email' onChange={(e) => setEmail(e.target.value)} required />
-                            <Components.Input type='password' placeholder='Password' onChange={(e) => setPassword(e.target.value)} required />
-                            <Components.Anchor href='#'>Forgot your password?</Components.Anchor>
-                            <Components.Button >Sigin In</Components.Button>
-                            <GoogleOAuthProvider clientId="446061591121-opbuk29t8r85tdq2uia6am4m54ioqleh.apps.googleusercontent.com">
-                                <div style={{ margin: 10 }}>
+            <ToastContainer
+                position="top-right"
+                autoClose={2500}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="colored"
+            />
+            <div className="mt-4 grow flex items-center justify-around ">
+                <div className="mb-64 shadow-2xl rounded-lg p-10 ">
+                    <h1 className="text-4xl text-center mb-4">Login</h1>
+                    <form className="max-w-md mx-auto" onSubmit={handleSubmit}>
+                        <div className='w-[400px]'>
+                            <div className='flex items-center rounded-2xl'>
+                                <MdMail className='mx-2'/>
+                                <input type="email"
+                                    placeholder="your@email.com"
+                                    value={email}
+                                    onChange={ev => setEmail(ev.target.value)} />
+                            </div>
+                            <p className='text-rose-700'>{formErrors.email}</p>
+                            <div className='flex items-center rounded-2xl'>
+                                <RiLockPasswordFill className='mx-2' />
+                                <input type="password"
+                                    placeholder="password"
+                                    value={password}
+                                    onChange={ev => setPassword(ev.target.value)} />
+                            </div>
+                            <p className='text-rose-700'>{formErrors.password}</p>
+                            <button className="primary">Login</button>
+                            <div className='py-3 flex justify-center text-sky-600 font-bold'>
+                                <Link to={'/forgot-password'}>Forgot Password</Link>
+                            </div>
+                            <GoogleOAuthProvider className='m-10 bg-danger' clientId="446061591121-opbuk29t8r85tdq2uia6am4m54ioqleh.apps.googleusercontent.com">
+                                <div className='m-5 flex justify-center'>
                                     <GoogleLogin
                                         onSuccess={credentialResponse => {
                                             var decoded = jwt_decode(credentialResponse.credential);
@@ -81,38 +103,15 @@ const Adminlogin = () => {
                                     />
                                 </div>
                             </GoogleOAuthProvider>
-                        </Components.Form>
-                    </Components.SignInContainer>
-
-                    <Components.OverlayContainer signinIn={signIn}>
-                        <Components.Overlay signinIn={signIn}>
-
-                            <Components.LeftOverlayPanel signinIn={signIn}>
-                                <Components.Title>Welcome Back!</Components.Title>
-                                <Components.Paragraph>
-                                    To keep connected with us please login with your personal info
-                                </Components.Paragraph>
-                                <Components.GhostButton onClick={() => toggle(true)}>
-                                    Sign In
-                                </Components.GhostButton>
-                            </Components.LeftOverlayPanel>
-
-                            <Components.RightOverlayPanel signinIn={signIn}>
-                                <Components.Title>Hello, Friend!</Components.Title>
-                                <Components.Paragraph>
-                                    Enter Your personal details and start journey with us
-                                </Components.Paragraph>
-                                <Components.GhostButton onClick={() => toggle(false)}>
-                                    Sigin Up
-                                </Components.GhostButton>
-                            </Components.RightOverlayPanel>
-
-                        </Components.Overlay>
-                    </Components.OverlayContainer>
-                </Components.Container>
-            </Components.Mcontainer>
+                            <div className="text-center py-2 text-gray-500">
+                                Don't have an account yet? <Link className="underline text-black" to={'/user-register'}>Register now</Link>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </>
     )
 }
 
-export default Adminlogin
+export default Login
