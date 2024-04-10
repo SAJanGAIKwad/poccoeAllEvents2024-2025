c4d94bd78295311d6403d0daac78c67ca8a51799
SAJanGAIKwad <sajan.gaikwad21@pccoepune.org>
2024-04-10 23:26:10 +0530

    Deployed SuccessFully

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import pluginRewriteAll from 'vite-plugin-rewrite-all';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), pluginRewriteAll()],
   base: "/PCCOE-ALL-Event-Frontend/",
+  plugins: [react(), pluginRewriteAll()],
+  build: { chunkSizeWarningLimit: 16000, }
 })
